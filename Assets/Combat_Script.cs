464153897be00d952955a54865988f2a417717fb
SimM20 <83238073+SimM20@users.noreply.github.com>
2024-07-04 14:59:20 -0300

    Fixed error with build packages

--- Assets/Combat_Script.cs
+++ Assets/Combat_Script.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 
 public enum BattleState{START, PLAYER, ENEMY, WIN, LOSE}
 
