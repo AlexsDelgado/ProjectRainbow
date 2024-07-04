74f861ec0929ba453893697e53a6d77e2db12e34
SimM20 <83238073+SimM20@users.noreply.github.com>
2024-07-04 00:48:36 -0300

    Added test scene and shield fx with fxmanager

---
+++ Assets/FXManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXManager : MonoBehaviour
+{
+    public FXManager Instance;
+    [SerializeField] private Animator player;
+    [SerializeField] private Animator enemy;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayPlayerAnimation(string trigger)
+    {
+        player.SetTrigger(trigger);
+    }
+    public void PlayEnemyAnimation(string trigger)
+    {
+        enemy.SetTrigger(trigger);
+    }
+}
